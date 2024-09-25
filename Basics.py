0a2ced1cc02321a74a734bf3dc00647297a7b6d5
RoopalAgarwal <roopalk04@gmail.com>
2024-09-25 21:32:15 +0530

    Basics

---
+++ Basics.py
@@ -0,0 +1,9 @@
+salary=25000
+name="Roopal Agarwal"
+email="roopalk04@gmail.com"
+age=19
+gender= "female"
+print(salary)
+print(name+" "+email,salary)
+
+print("My name is "+name+ " my age is ",age,"my gender is "+gender+ " my email is "+email)
