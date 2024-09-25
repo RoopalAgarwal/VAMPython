55adf2d854902a913c286592bce825738a6bc15a
RoopalAgarwal <roopalk04@gmail.com>
2024-09-25 20:32:30 +0530

    my projects

---
+++ Firstprog.py
@@ -0,0 +1,21 @@
+salary=25000
+name="Roopal Agarwal"
+email="roopalk04@gmail.com"
+age=19
+gender= "female"
+print(salary)
+print(name+" "+email,salary)
+
+
+#To take input from user
+collegename = input("Enter college name:")
+print(collegename)
+
+firstname=input("Enter first name: ")
+lastname=input("Enter last name: ")
+print(firstname+" "+lastname)
+
+print("My name is "+name+ " my age is ",age,"my gender is "+gender+ " my email is "+email)
+
+
+
