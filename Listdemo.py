c9c664920b402392c5b61f6a711dbdcc55bd199d
RoopalAgarwal <roopalk04@gmail.com>
2024-09-26 11:59:00 +0530

    Lists

---
+++ Listdemo.py
@@ -0,0 +1,16 @@
+# list can store multiple data, data can be of different types od data like int, str, float
+# list can store the duplicate data
+# list is an ordered data set - sorting (Ascending or descending)
+
+# create list and store friends name
+friendlist = ["Kashish, Rani, Tripti, Shreya"]
+
+# print the list of friends name
+print(friendlist)
+
+# add the age of your friend into list
+friendlist.append(18)         # append will add the data into the end of the list
+friendlist.append(20)
+friendlist.append(19)
+friendlist.append(20)
+print(friendlist)
